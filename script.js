8f1b51e7e48e3292c9337a097059dbdf6b9e1164
Lorenzo Boccuzzi <lorenzo.boccuzzi@studio.unibo.it>
2025-03-27 23:13:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ function convertUrlToUri() {
     if (parts.length < 5 || parts[2] !== "open.spotify.com") {
         document.getElementById("outputUri").innerHTML = 'Invalid Spotify URL.';
         return;
-    }document.getElementById("outputUri").innerHTML = "CLICCA SULL'IMMAGINE PER SCARICARE L'SVG";
+    } else {
+        document.getElementById("outputUri").innerHTML = "CLICCA SULL'IMMAGINE PER SCARICARE L'SVG";
+    }
+
     var type = parts[3];
     var id = parts[4].split('?')[0]; // Remove any query parameters
     var uri = 'spotify:' + type + ':' + id;

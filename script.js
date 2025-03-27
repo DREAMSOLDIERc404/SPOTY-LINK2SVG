2a42fc1b19d643072bdecbc3f2addbcbae9c6232
Lorenzo Boccuzzi <lorenzo.boccuzzi@studio.unibo.it>
2025-03-28 00:57:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+function cambiaTesto() {
+    document.getElementById("demo").innerHTML = "Hai cliccato il bottone!";
+}
+
+function convertUrlToUri() {
+    var url = document.getElementById("spotifyLink").value; // Cambia l'ID a "spotifyLink"
+    var parts = url.split('/');
+    if (parts.length < 5 || parts[2] !== "open.spotify.com") {
+        document.getElementById("outputUri").innerHTML = 'Invalid Spotify URL.';
+        return;
+    } else {
+        document.getElementById("outputUri").innerHTML = "CLICCA SULL'IMMAGINE PER SCARICARE L'SVG";
+    }
+
+    var type = parts[3];
+    var id = parts[4].split('?')[0]; // Remove any query parameters
+    var uri = 'spotify:' + type + ':' + id;
+    displaySpotifyCode(uri);
+    console.log("Spotify URI generato: " + uri); // Aggiungi una stampa per la console
+}
+
+function displaySpotifyCode(uri) {
+    var spotifyCodeUrl = `https://scannables.scdn.co/uri/plain/png/000000/white/1000/${encodeURIComponent(uri)}`;
+    var img = document.createElement('img');
+    img.src = spotifyCodeUrl;
+    img.alt = 'Spotify Code';
+    img.style.width = '320px'; // Modifica la larghezza a 320px
+    img.style.height = 'auto';
+    img.style.cursor = 'pointer'; // Change cursor to pointer to indicate clickability
+    img.addEventListener('click', function() {
+        convertImageToSVG(spotifyCodeUrl, 'spotify_code.svg');
+    });
+
+    var outputDiv = document.getElementById("outputUri");
+    outputDiv.innerHTML = "CLICCA SULL'IMMAGINE PER SCARICARE L'SVG"; // Clear previous content and add text
+    
+    var previewDiv = document.getElementById("preview");
+    previewDiv.innerHTML = ''; // Clear previous content
+    previewDiv.appendChild(img);
+}
+
 function convertImageToSVG(url, filename) {
     var img = new Image();
     img.crossOrigin = 'anonymous';
@@ -26,4 +67,6 @@ function convertImageToSVG(url, filename) {
 }
 
 // Aggiungi un listener per il bottone di submit
+document.getElementById('submitButton').addEventListener('click', convertUrlToUri);
+
 document.getElementById('submitButton').addEventListener('click', convertUrlToUri);

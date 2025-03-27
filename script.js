fd660f96ba04ad3de7dc15de4b2a3d1b92285b57
Lorenzo Boccuzzi <lorenzo.boccuzzi@studio.unibo.it>
2025-03-27 15:05:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,17 @@ function convertUrlToUri() {
     var type = parts[3];
     var id = parts[4].split('?')[0]; // Remove any query parameters
     var uri = 'spotify:' + type + ':' + id;
-    document.getElementById("outputUri").innerHTML = uri;
+    downloadSpotifyCode(uri);
+}
+
+function downloadSpotifyCode(uri) {
+    var spotifyCodeUrl = `https://scannables.scdn.co/uri/plain/png/000000/white/1000/${encodeURIComponent(uri)}`;
+    var link = document.createElement('a');
+    link.href = spotifyCodeUrl;
+    link.download = 'spotify_code.png';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
 
 document.getElementById('submitButton').addEventListener('click', convertUrlToUri);

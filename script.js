421dec870b6b360d84f83d4ad058e3a6862f65fe
Lorenzo Boccuzzi <lorenzo.boccuzzi@studio.unibo.it>
2025-03-27 15:21:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,17 +12,19 @@ function convertUrlToUri() {
     var type = parts[3];
     var id = parts[4].split('?')[0]; // Remove any query parameters
     var uri = 'spotify:' + type + ':' + id;
-    downloadSpotifyCode(uri);
+    displaySpotifyCode(uri);
 }
 
-function downloadSpotifyCode(uri) {
+function displaySpotifyCode(uri) {
     var spotifyCodeUrl = `https://scannables.scdn.co/uri/plain/png/000000/white/1000/${encodeURIComponent(uri)}`;
-    var link = document.createElement('a');
-    link.href = spotifyCodeUrl;
-    link.download = 'spotify_code.png';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    var img = document.createElement('img');
+    img.src = spotifyCodeUrl;
+    img.alt = 'Spotify Code';
+    img.style.width = '1000px';
+    img.style.height = 'auto';
+    var outputDiv = document.getElementById("outputUri");
+    outputDiv.innerHTML = ''; // Clear previous content
+    outputDiv.appendChild(img);
 }
 
 document.getElementById('submitButton').addEventListener('click', convertUrlToUri);

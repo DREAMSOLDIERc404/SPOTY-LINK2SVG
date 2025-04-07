cda6c25da18266507076fb139cde921d9b45c239
Lorenzo Boccuzzi <lorenzo.boccuzzi@studio.unibo.it>
2025-04-07 11:54:31 +0200

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,6 +1,5 @@
-
 function convertUrlToUri() {
-  console.log("Funzione convertUrlToUri chiamaa");
+  console.log("Funzione convertUrlToUri chiamata");
   var url = document.getElementById("spotifyLink").value;
   console.log("URL inserito:", url);
   var parts = url.split('/');
@@ -11,12 +10,12 @@ function convertUrlToUri() {
   } else {
     document.getElementById("outputUri").innerHTML = "CLICCA SULL'IMMAGINE PER SCARICARE L'SVG";
   }
-  if(parts[3]== "intl-it"){
+  if (parts[3] == "intl-it") {
     var type = parts[4];
     var id = parts[5].split('?')[0];
-  }else{
+  } else {
     var type = parts[3];
-    var id = parts[4].split('?')[0];  
+    var id = parts[4].split('?')[0];
   }
   var uri = 'spotify:' + type + ':' + id;
   console.log("Spotify URI generato:", uri);
@@ -68,3 +67,37 @@ async function convertImageToSVG(url, filename) {
 }
 
 document.getElementById('submitButton').addEventListener('click', convertUrlToUri);
+Modifica del file server.js
+Aggiungiamo dei messaggi di debug alla console nel file server.js:
+
+const potrace = require('potrace');
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+async function convertImageToSVG(url, filename) {
+  console.log("Funzione convertImageToSVG chiamata con URL:", url);
+  try {
+    const response = await fetch(url);
+    const buffer = await response.buffer();
+    const tempFile = 'temp.png';
+    fs.writeFileSync(tempFile, buffer);
+    potrace.trace(tempFile, { turdSize: 100, alphaMax: 0.4 }, function (err, svg) {
+      if (err) {
+        console.error("Errore durante la conversione dell'immagine in SVG:", err);
+        throw err;
+      }
+      var blob = new Blob([svg], { type: 'image/svg+xml' });
+      var a = document.createElement('a');
+      a.href = URL.createObjectURL(blob);
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      fs.unlinkSync(tempFile);
+    });
+  } catch (err) {
+    console.error("Errore durante la richiesta di fetch:", err);
+  }
+}
+
+module.exports = { convertImageToSVG };

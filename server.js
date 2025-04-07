e2eb67c35555392e64297ff9ef8d928f3999924e
Lorenzo Boccuzzi <lorenzo.boccuzzi@studio.unibo.it>
2025-04-07 16:56:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,33 @@
 const express = require('express');
 const potrace = require('potrace');
-const fs = require('fs');
 const fetch = require('node-fetch');
+const { promisify } = require('util');
+
+// "Promisificare" potrace.trace
+const trace = promisify(potrace.trace);
 
 const app = express();
 
-async function convertImageToSVG(url, filename) {
+async function convertImageToSVG(url) {
   console.warn("Funzione convertImageToSVG chiamata con URL:", url);
   try {
     const response = await fetch(url);
+    if (!response.ok) throw new Error("Errore durante il fetch dell'immagine");
     const buffer = await response.buffer();
-    const tempFile = 'temp.png';
-    fs.writeFileSync(tempFile, buffer);
-    potrace.trace(tempFile, { turdSize: 100, alphaMax: 0.4 }, function (err, svg) {
-      if (err) {
-        console.error("Errore durante la conversione dell'immagine in SVG:", err);
-        throw err;
-      }
-      fs.unlinkSync(tempFile);
-      return svg;
-    });
+
+    // Converti direttamente il buffer in SVG
+    const svg = await trace(buffer, { turdSize: 100, alphaMax: 0.4 });
+    return svg;
   } catch (err) {
-    console.error("Errore durante la richiesta di fetch:", err);
+    console.error("Errore durante la conversione dell'immagine in SVG:", err);
     throw err;
   }
 }
 
 app.get('/api/convert', async (req, res) => {
   const url = decodeURIComponent(req.query.url);
-  const filename = req.query.filename;
   try {
-    const svg = await convertImageToSVG(url, filename);
+    const svg = await convertImageToSVG(url);
     res.setHeader('Content-Type', 'image/svg+xml');
     res.send(svg);
   } catch (err) {
@@ -38,6 +35,7 @@ app.get('/api/convert', async (req, res) => {
   }
 });
 
+// Porta 3000 per lo sviluppo locale
 app.listen(3000, () => {
   console.log('Server in ascolto sulla porta 3000');
 });

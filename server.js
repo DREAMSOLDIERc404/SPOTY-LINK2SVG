ea3f633a2e524c3d5bd4307712d0f2384689259c
Lorenzo Boccuzzi <lorenzo.boccuzzi@studio.unibo.it>
2025-04-07 15:59:11 +0200

    Server.js tolto il download del file di log

--- server.js
+++ server.js
@@ -4,29 +4,24 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 
 const app = express();
-const logFile = 'server.log';
 
 async function convertImageToSVG(url, filename) {
-  fs.appendFileSync(logFile, `Funzione convertImageToSVG chiamata con URL: ${url}\n`);
+  warn.log("Funzione convertImageToSVG chiamata con URL:", url);
   try {
     const response = await fetch(url);
-    if (!response.ok) {
-      fs.appendFileSync(logFile, `Errore durante il fetch dell'immagine: ${response.statusText}\n`);
-      throw new Error(`Errore durante il fetch dell'immagine: ${response.statusText}`);
-    }
     const buffer = await response.buffer();
     const tempFile = 'temp.png';
     fs.writeFileSync(tempFile, buffer);
     potrace.trace(tempFile, { turdSize: 100, alphaMax: 0.4 }, function (err, svg) {
       if (err) {
-        fs.appendFileSync(logFile, `Errore durante la conversione dell'immagine in SVG: ${err}\n`);
+        console.error("Errore durante la conversione dell'immagine in SVG:", err);
         throw err;
       }
       fs.unlinkSync(tempFile);
       return svg;
     });
   } catch (err) {
-    fs.appendFileSync(logFile, `Errore durante la richiesta di fetch: ${err}\n`);
+    console.error("Errore durante la richiesta di fetch:", err);
     throw err;
   }
 }
@@ -34,19 +29,10 @@ async function convertImageToSVG(url, filename) {
 app.get('/convert', async (req, res) => {
   const url = decodeURIComponent(req.query.url);
   const filename = req.query.filename;
-  fs.appendFileSync(logFile, `URL decodificato: ${url}\n`);
   try {
     const svg = await convertImageToSVG(url, filename);
     res.setHeader('Content-Type', 'image/svg+xml');
     res.send(svg);
-
-    // Invio del file di log al client
-    res.download(logFile, 'server.log', (err) => {
-      if (err) {
-        console.error("Errore durante il download del file di log:", err);
-        res.status(500).send('Errore durante il download del file di log');
-      }
-    });
   } catch (err) {
     res.status(500).send('Errore durante la conversione dell\'immagine in SVG');
   }

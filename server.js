9aa611f80bb9cc94049d47bd3b97a3bedabfbb88
Lorenzo Boccuzzi <lorenzo.boccuzzi@studio.unibo.it>
2025-04-07 18:06:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ async function convertImageToSVG(url) {
   }
 }
 
-app.get('convert', async (req, res) => {
+app.get('/api/convert', async (req, res) => {
   const url = decodeURIComponent(req.query.url);
   try {
     const svg = await convertImageToSVG(url);

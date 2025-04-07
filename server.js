1f78986f9abfbf1854d438108c10d8b8ae2fb95a
Lorenzo Boccuzzi <lorenzo.boccuzzi@studio.unibo.it>
2025-04-07 11:21:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 
 async function convertImageToSVG(url, filename) {
+  console.log("Funzione convertImageToSVG chiamata con URL:", url);
   try {
     const response = await fetch(url);
     const buffer = await response.buffer();
     const tempFile = 'temp.png';
     fs.writeFileSync(tempFile, buffer);
     potrace.trace(tempFile, { turdSize: 100, alphaMax: 0.4 }, function (err, svg) {
-      if (err) throw err;
+      if (err) {
+        console.error("Errore durante la conversione dell'immagine in SVG:", err);
+        throw err;
+      }
       var blob = new Blob([svg], { type: 'image/svg+xml' });
       var a = document.createElement('a');
       a.href = URL.createObjectURL(blob);
@@ -20,7 +24,7 @@ async function convertImageToSVG(url, filename) {
       fs.unlinkSync(tempFile);
     });
   } catch (err) {
-    console.error(err);
+    console.error("Errore durante la richiesta di fetch:", err);
   }
 }
 
